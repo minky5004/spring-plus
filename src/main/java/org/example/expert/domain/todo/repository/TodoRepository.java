3fd8b19116c56908a8c33352b1c114014ddbed4a
minky5004 <minky5004@gmail.com>
2026-03-03 20:51:17 +0900

    Level.1 - 3 해결

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,14 +7,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-
-
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    @Query("SELECT t FROM Todo t " +
+            "LEFT JOIN FETCH t.user u " +
+            "WHERE (:weather IS NULL OR t.weather = :weather) " +
+            "AND (:startDate IS NULL OR :endDate IS NULL OR t.modifiedAt BETWEEN :startDate AND :endDate) " +
+            "AND (:startDate IS NOT NULL OR :endDate IS NULL OR t.modifiedAt <= :endDate) " +
+            "AND (:endDate IS NOT NULL OR :startDate IS NULL OR t.modifiedAt >= :startDate) " +
+            "ORDER BY t.modifiedAt DESC")
+    Page<Todo> findByCondition(
+            @Param("weather") String weather,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate,
+            Pageable pageable
+    );
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
