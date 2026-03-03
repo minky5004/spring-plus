3ba1a5262cecda4efdafaaabfaa198840be37d46
minky5004 <minky5004@gmail.com>
2026-03-03 23:32:30 +0900

    Level. 2 - 4 해결

---
+++ src/main/java/org/example/expert/config/JwtAuthenticationFilter.java
@@ -0,0 +1,62 @@
+package org.example.expert.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  private final JwtUtil jwtUtil;
+
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    String authHeader = request.getHeader("Authorization");
+
+    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+      String jwt = authHeader.substring(7);
+      try {
+        Claims claims = jwtUtil.extractClaims(jwt);
+        Long userId = Long.parseLong(claims.getSubject());
+        String email = claims.get("email", String.class);
+        String nickname = claims.get("nickname", String.class);
+        UserRole userRole = UserRole.valueOf(claims.get("userRole", String.class));
+
+        if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+          AuthUser authUser = new AuthUser(userId, email, nickname, userRole);
+
+          UsernamePasswordAuthenticationToken authToken =
+              new UsernamePasswordAuthenticationToken(authUser, null, authUser.getAuthorities());
+
+          authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+          SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+      } catch (ExpiredJwtException e) {
+        // 1. 만료된 경우 (가장 자주 발생함)
+        log.error("Expired JWT token, 만료된 JWT 토큰입니다.", e);
+      } catch (Exception e) {
+        // 2. 그 외 모든 에러 (잘못된 서명, 지원되지 않는 형식, 빈 토큰 등)
+        log.error("Invalid JWT token, 유효하지 않은 JWT 토큰입니다. 원인: {}", e.getMessage());
+      }
+    }
+  }
+}
